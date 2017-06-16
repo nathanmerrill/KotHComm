5096f69cee5b7064bf1a06cc2ff4bf084dde605a
Nathan Merrill <nathan.t.merrill@gmail.com>
2017-06-16 15:27:35 -0600

    Added remaining duration to TextUI

--- src/main/java/com/nmerrill/kothcomm/ui/text/TextUI.java
+++ src/main/java/com/nmerrill/kothcomm/ui/text/TextUI.java
@@ -6,11 +6,17 @@ import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.impl.factory.Lists;
 
 import java.io.PrintStream;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class TextUI {
     public final PrintStream out;
     private boolean showScores;
+    private Instant start;
     public TextUI(PrintStream out){
         this.out = out;
         showScores = true;
@@ -55,7 +61,7 @@ public class TextUI {
         out.println(builder.display(table));
     }
 
-    public void printScoreboard(Scoreboard scoreboard){
+    public void printScoreboard(Scoreboard<?> scoreboard){
         TableBuilder builder = new TableBuilder();
         builder.rightAlign(0);
         builder.rightAlign(2);
@@ -64,8 +70,24 @@ public class TextUI {
         printScoreboard(scoreboard, builder);
     }
 
+    private Duration timeRemaining(int current, int max){
+        if (start == null){
+            start = Instant.now();
+            return Duration.ofMillis(0);
+        }
+        Instant currentTime = Instant.now();
+        Duration duration = Duration.between(start, currentTime);
+        return duration.dividedBy(current).multipliedBy(max-current);
+    }
+
     public void printProgress(int current, int max){
         int percent = current * 100/max;
+        Duration remaining = timeRemaining(current, max);
+        long hours = remaining.toHours();
+        remaining = remaining.minus(hours, HOURS);
+        long minutes = remaining.toMinutes();
+        remaining = remaining.minus(minutes, MINUTES);
+        long seconds = remaining.getSeconds();
         out.print(
                 "\r" +
                 String.format(" %d%% [", percent) +
@@ -73,7 +95,8 @@ public class TextUI {
                 '>' +
                 repeated(' ', 100 - percent) +
                 "] " +
-                current + '/' + max
+                current + '/' + max +
+                " " + hours + "h " + minutes + "m " + seconds + "s"
         );
     }
 
